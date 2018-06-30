077f74932b57d66605ab7981ebf4fea273ed1294
manisha <kashyapmanisha29@gmail.com>
2018-06-30 14:09:37 +0530

    Read json file

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require ("express");
+const myParser = require("body-parser");
+const app= express();
+//const f = require("middle");
+app.use(myParser.json());
+app.post('/mypath',(req,res)=>{
+	const data = req.body;
+	console.log(data);
+	//console.log(data.Query);
+	const response = {
+		text: "The code is working fine"
+	}
+	res.json(response);
+
+});
+
+
+app.listen(3000);
+
